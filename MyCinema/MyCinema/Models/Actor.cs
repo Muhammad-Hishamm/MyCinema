00e2bf85e49f7559f7756a935a1b0eab95783a07
Muhammad Hisham <72977798+Muhammad-Hishamm@users.noreply.github.com>
2025-06-29 04:09:22 +0300

    Add:Creating a project and adding the Actor model

---
+++ MyCinema/MyCinema/Models/Actor.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyCinema.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Bio { get; set; }
+
+        public string ProfirlePictureUrl { get; set; }
+    }
+}
