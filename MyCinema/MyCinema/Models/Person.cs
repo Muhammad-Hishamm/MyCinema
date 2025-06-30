b853e83acf39d6f0ae7221a482dd794292a14455
Muhammad Hisham <72977798+Muhammad-Hishamm@users.noreply.github.com>
2025-07-01 01:51:53 +0300

    feat: add Person Model which the parent of Actor and producer models

---
+++ MyCinema/MyCinema/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyCinema.Models
+{
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+        public string ProfirlePictureUrl { get; set; }
+
+    }
+
+}
