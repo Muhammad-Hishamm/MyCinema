45ad9edd28238e33c74f71e79c970964d430ccd2
Muhammad Hisham <72977798+Muhammad-Hishamm@users.noreply.github.com>
2025-07-01 01:39:32 +0300

    feat: add Movie Model

---
+++ MyCinema/MyCinema/Models/Movie.cs
@@ -0,0 +1,33 @@
+﻿using MyCinema.Data.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyCinema.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateOnly StarDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string ImageUrl { get; set; }
+        public MovieCategory Category { get; set; }
+
+
+        //Relationships
+
+        // Producer
+        public int ProducerId { get; set; }
+        [ForeignKey("ProducerId")]
+        public Producer Producer { get; set; }
+
+        //Actors
+        public List<Actor_Movie> Actors { get; set; }
+
+        //Cinema
+        public int CinemaId { get; set; }
+        [ForeignKey("CinemaId")]
+        public Cinema Cinema { get; set; }
+    }
+}
